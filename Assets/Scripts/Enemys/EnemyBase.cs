d69534af141040090d8001a969cd272acb53450e
HeitorPires <heitorpprocopio@hotmail.com>
2024-08-24 15:00:05 -0400

    Add born animation

--- Assets/Scripts/Enemys/EnemyBase.cs
+++ Assets/Scripts/Enemys/EnemyBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Enemy
 {
@@ -8,6 +9,11 @@ namespace Enemy
     public class EnemyBase : MonoBehaviour
     {
         public float startLife = 10;
+
+        [Header("Start Animation")]
+        public float startAnimationDuration = .2f;
+        public Ease startAnimationEase = Ease.OutBack;
+        public bool startWithBornAnimation = true;
 
         [SerializeField]private float _currentLife;
 
@@ -19,6 +25,7 @@ namespace Enemy
         protected virtual void Init()
         {
             ResetLife();
+            if(startWithBornAnimation) BornAnimation();
         }
 
         protected void ResetLife()
@@ -42,6 +49,13 @@ namespace Enemy
             if( _currentLife <= 0 )
                 OnKill();
         }
+
+        #region ANIMATION
+        private void BornAnimation()
+        {
+            transform.DOScale(0, startAnimationDuration).From().SetEase(startAnimationEase);
+        }
+        #endregion
 
         #region debug
         private void Update()
