bd015d6651f28f0442965040ed7110cace63a441
HeitorPires <heitorpprocopio@hotmail.com>
2024-08-26 17:45:52 -0400

    Add CheckpointBase

---
+++ Assets/Scripts/Checkpoint/CheckpointBase.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointBase : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public int key = 01;
+    private string checkpointKey = "CheckpointKey";
+    private bool checkpointSaved = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!checkpointSaved &&other.transform.CompareTag("Player"))
+        {
+            CheckpointCheck();
+        }
+    }
+
+    [NaughtyAttributes.Button]  
+    private void CheckpointCheck()
+    {
+        TurnItOn();
+        SaveCheckpoint();
+    }
+
+    private void TurnItOn()
+    {
+        if (meshRenderer != null) meshRenderer.material.SetColor("_EmissionColor", Color.white);
+    }
+    
+    private void TurnItOff()
+    {
+        if (meshRenderer != null) meshRenderer.material.SetColor("_EmissionColor", Color.black);
+    }
+
+    private void SaveCheckpoint()
+    {
+        if(PlayerPrefs.GetInt(checkpointKey, 0) < key)
+            PlayerPrefs.SetInt(checkpointKey, key);
+
+        checkpointSaved = true;
+    }
+
+}
