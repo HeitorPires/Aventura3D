c74b1ebd23894e649b3394a597450a2b6f617cc8
HeitorPires <heitorpprocopio@hotmail.com>
2024-08-28 17:00:34 -0400

    Add chest coins

--- Assets/Scripts/Chest/ChestBase.cs
+++ Assets/Scripts/Chest/ChestBase.cs
@@ -8,11 +8,17 @@ public class ChestBase : MonoBehaviour
     public string triggerOpen = "Open";
     public string tagToCompare = "Player";
     public GameObject notification;
+    public KeyCode keyCode = KeyCode.F;
 
     [Header("Animation")]
     public float animationDuration = 1f;
     public Ease ease = Ease.OutBounce;
     public float startScale;
+
+    [Space]
+    public ChestItemBase chestItem; 
+
+    private bool _isChestOpened;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +27,33 @@ public class ChestBase : MonoBehaviour
         startScale = notification.transform.localScale.x;
     }
 
+    private void Update()
+    {
+        if(Input.GetKeyDown(keyCode))
+        {
+            OpenChest();
+        }
+    }
+
     [NaughtyAttributes.Button]
     private void OpenChest()
     {
+        if (_isChestOpened) return;
         Animator.SetTrigger(triggerOpen);
+        _isChestOpened = true;
+        HideNotification();
+        Invoke(nameof(ShowItem), 1f);
+        Invoke(nameof(CollectItems), 2f);
+    }
+
+    private void ShowItem()
+    {
+        chestItem.ShowItem();
+    }
+    
+    private void CollectItems()
+    {
+        chestItem.Collect();
     }
 
     public void OnTriggerEnter(Collider other)
@@ -46,6 +75,7 @@ public class ChestBase : MonoBehaviour
     [NaughtyAttributes.Button]
     private void ShowNotification()
     {
+        if (_isChestOpened) return;
         notification.SetActive(true);
         notification.transform.localScale = Vector3.zero;
         notification.transform.DOScale(startScale, animationDuration).SetEase(ease);
---
+++ Assets/Scripts/Chest/ChestItemCoin.cs
@@ -0,0 +1,50 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Items;
+
+public class ChestItemCoin : ChestItemBase
+{
+    public int coinAmount = 5;
+    public GameObject coinObject;
+    public Vector2 randomRange = new Vector2(-2f, 2f);
+
+    [Header("Animation")]
+    public float animationDuration = .2f;
+    public Ease ease = Ease.OutBack;
+
+    private List<GameObject> _itens = new List<GameObject>();
+
+    public override void ShowItem()
+    {
+        base.ShowItem();
+        CreateItens();
+    }
+
+    [NaughtyAttributes.Button]
+    private void CreateItens()
+    {
+        for (int i = 0; i < coinAmount; i++)
+        {
+            var item = Instantiate(coinObject);
+            item.transform.position = transform.position + Random.Range(randomRange.x ,randomRange.y) * Vector3.forward + Random.Range(randomRange.x, randomRange.y) * Vector3.right;
+            item.transform.DOScale(0, animationDuration).From().SetEase(ease);
+            _itens.Add(item);
+        }
+    }
+
+    [NaughtyAttributes.Button]
+    public override void Collect()
+    {
+        base.Collect();
+        foreach (var item in _itens)
+        {
+            item.transform.DOMoveY(2f, animationDuration).SetRelative();
+            item.transform.DOScale(0, animationDuration / 2).SetDelay(animationDuration / 2);
+            ItemManager.Instance.AddByType(ItemType.COIN);
+        }
+        _itens.Clear();
+    }
+
+}
